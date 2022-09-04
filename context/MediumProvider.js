2f6f7d0e125efe36f0ac6ec6d91d18b319fb1382
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2022-09-04 13:22:08 +0530

    updating login fuctionality

--- context/MediumProvider.js
+++ context/MediumProvider.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
+import { db } from "../firebase";
 
-const MediumContext = createContext(null);
+export const MediumContext = createContext(null);
 
 const MediumProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [posts, setPosts] = useState([]);
-  return <div></div>;
+  const contextValue = { users, posts };
+  useEffect(() => {
+    const getUsers = async () => {
+      const querySnapShot = await getDocs(collection(db, "articles"));
+      querySnapShot.docs.map((doc) => console.log(doc.data()));
+    };
+    getUsers();
+  }, []);
+  return (
+    <>
+      <MediumContext.Provider value={contextValue}>
+        {children}
+      </MediumContext.Provider>
+    </>
+  );
 };
 
 export default MediumProvider;
